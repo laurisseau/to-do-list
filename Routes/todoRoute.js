636c9c50ba42c16163da90ae5af1d679ebd20cb9
laurisseau Joseph <reso0208@gmail.com>
2022-10-03 12:22:22 -0400

    Update todoRoute.js

--- Routes/todoRoute.js
+++ Routes/todoRoute.js
@@ -1,6 +1,29 @@
 const router = require("express").Router()
 const Todo = require('../Models/todoModel.js')
 
+router.get("/read/todo", async(req, res) =>{
+    
+    try{
+
+        const readTodo = await Todo.find()
+
+        res.status(201).json({
+
+            status: 'success',
+            data:{
+                readTodo
+            }
+
+        })
+
+    }catch(err){
+        
+        console.log(err)
+
+    }
+
+})
+
 router.post("/add/todo", async(req, res) =>{
     
     try{
@@ -22,5 +45,24 @@ router.post("/add/todo", async(req, res) =>{
 
 })
 
+router.delete("/delete/todo/:id", async(req, res) =>{
+    
+    try{
+
+        const deleteTodo = await Todo.findByIdAndDelete(req.params.id)
+
+        res.status(201).json({
+
+            status: 'success'
+
+        })
+
+    }catch(err){
+        
+        console.log(err)
+
+    }
+
+})
 
 module.exports = router
